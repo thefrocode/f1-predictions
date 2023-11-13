b339e5720079a1d556bf15af9fb3407e61292169
The Fro Code <cmukiri808@gmail.com>
2023-11-13 14:37:27 +0300

    Initial Commit

--- apps/f1-predictions/src/app/app.component.ts
+++ apps/f1-predictions/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, HlmButtonDirective],
   selector: 'f1-predictions-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
---
+++ libs/shared/ui/ui-button-helm/README.md
@@ -0,0 +1,9 @@
+# shared-ui-ui-button-helm
+
+This library was generated with [Nx](https://nx.dev).
+
+
+## Running unit tests
+
+Run `nx test shared-ui-ui-button-helm` to execute the unit tests.
+
