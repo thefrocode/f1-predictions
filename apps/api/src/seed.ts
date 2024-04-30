bfc8a2b80f5c5e85a179af47acab64d65ce9e12f
The Fro Code <cmukiri808@gmail.com>
2024-05-01 00:26:13 +0300

    Run seed on prod

--- apps/api/src/seed.ts
+++ apps/api/src/seed.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import * as fs from 'fs';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { runSeeders, SeederOptions } from 'typeorm-extension';
 import { Driver } from './app/drivers/entities/driver.entity';
@@ -12,6 +13,7 @@ import { Result } from './app/predictions/entities/result.entity';
 import { Team } from './app/predictions/entities/team.entity';
 import { Race } from './app/races/entities/race.entity';
 import MainSeeder from './app/shared/seeders/main.seeder';
+import { configService } from './config/config.service';
 
 const {
   F1_MYSQL_HOST,
@@ -19,6 +21,7 @@ const {
   F1_MYSQL_USER,
   F1_MYSQL_PASSWORD,
   F1_MYSQL_DATABASE,
+  MODE,
 } = process.env;
 
 function getValue(value: string | undefined, throwOnMissing = true): string {
@@ -28,29 +31,63 @@ function getValue(value: string | undefined, throwOnMissing = true): string {
   return value as string;
 }
 
-const options: DataSourceOptions & SeederOptions = {
-  type: 'mysql',
-  host: F1_MYSQL_HOST,
-  port: parseInt(getValue(F1_MYSQL_PORT)),
-  username: F1_MYSQL_USER,
-  password: F1_MYSQL_PASSWORD,
-  database: F1_MYSQL_DATABASE,
-  entities: [
-    Player,
-    League,
-    LeaguePlayer,
-    Team,
-    Prediction,
-    Driver,
-    PredictionType,
-    Race,
-    Result,
-  ],
-  // additional config options brought by typeorm-extension
-  factories: [PlayersFactory],
-  seeds: [MainSeeder],
-};
-
+const options =
+  MODE === 'PROD'
+    ? ({
+        type: 'mysql',
+        host: F1_MYSQL_HOST,
+        port: parseInt(getValue(F1_MYSQL_PORT)),
+        username: F1_MYSQL_USER,
+        password: F1_MYSQL_PASSWORD,
+        database: F1_MYSQL_DATABASE,
+        synchronize: true,
+        logging: false,
+        ssl: false,
+        extra: {
+          ssl: {
+            rejectUnauthorized: true,
+            ca: fs.readFileSync('/etc/secrets/ca_cert').toString(),
+          },
+        },
+        entities: [
+          Player,
+          League,
+          LeaguePlayer,
+          Team,
+          Prediction,
+          Driver,
+          PredictionType,
+          Race,
+          Result,
+        ],
+        factories: [PlayersFactory],
+        seeds: [MainSeeder],
+      } as DataSourceOptions & SeederOptions)
+    : ({
+        type: 'mysql',
+        host: F1_MYSQL_HOST,
+        port: parseInt(getValue(F1_MYSQL_PORT)),
+        username: F1_MYSQL_USER,
+        password: F1_MYSQL_PASSWORD,
+        database: F1_MYSQL_DATABASE,
+        synchronize: true,
+        logging: false,
+        ssl: false,
+        entities: [
+          Player,
+          League,
+          LeaguePlayer,
+          Team,
+          Prediction,
+          Driver,
+          PredictionType,
+          Race,
+          Result,
+        ],
+        factories: [PlayersFactory],
+        seeds: [MainSeeder],
+      } as DataSourceOptions & SeederOptions);
+console.log(options);
 const dataSource = new DataSource(options);
 
 dataSource.initialize().then(async () => {
