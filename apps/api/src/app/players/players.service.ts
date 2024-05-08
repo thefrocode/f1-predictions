0c2455aad17241c85f0ebcb50381e3a0f145e502
The Fro Code <cmukiri808@gmail.com>
2024-05-08 04:16:16 +0300

    on creating player add them to the global league

--- apps/api/src/app/players/players.service.ts
+++ apps/api/src/app/players/players.service.ts
@@ -5,6 +5,7 @@ import { CreatePlayerDto } from './dto/create-player.dto';
 import { UpdatePlayerDto } from './dto/update-player.dto';
 import { Player } from './entities/player.entity';
 import { LeaguePlayer } from '../leagues/entities/league_player.entity';
+import { Prediction } from '../predictions/entities/prediction.entity';
 
 @Injectable()
 export class PlayersService {
@@ -14,9 +15,19 @@ export class PlayersService {
   @InjectRepository(LeaguePlayer)
   private readonly leaguePlayersRepository: Repository<LeaguePlayer>;
 
+  @InjectRepository(Prediction)
+  private readonly predictionsRepository: Repository<Prediction>;
+
   async create(createPlayerDto: CreatePlayerDto) {
     const newPlayer = this.playersRepository.create(createPlayerDto);
     await this.playersRepository.insert(newPlayer);
+
+    const leaguePlayer = this.leaguePlayersRepository.create({
+      player_id: newPlayer.id,
+      league_id: 1,
+    });
+
+    await this.leaguePlayersRepository.insert(leaguePlayer);
     return {
       message: 'Player created successfully',
     };
@@ -32,8 +43,34 @@ export class PlayersService {
     });
   }
 
-  findOne(id: number) {
-    return this.playersRepository.findOneBy({ id });
+  async findOne(user_id: string) {
+    const player = await this.playersRepository.findOneBy({ user_id });
+    if (player) {
+      const total_points = await this.predictionsRepository.sum('points', {
+        player_id: player.id,
+      });
+      const last_race_id = await this.predictionsRepository.maximum('race_id', {
+        player_id: player.id,
+      });
+      let last_race_points;
+      if (last_race_id) {
+        last_race_points = await this.predictionsRepository.sum('points', {
+          player_id: player.id,
+          race_id: last_race_id,
+        });
+      }
+      console.log({
+        total_points: total_points,
+        last_race_points: last_race_points,
+        player_id: player.id,
+      });
+      return {
+        total_points: total_points,
+        last_race_points: last_race_points,
+        player_id: player.id,
+      };
+    }
+    return new Error('Player not found');
   }
 
   findOneByUserId(user_id: string) {
